6b00c9548bb0cca268f088e6f5977003475570e2
Dean Iverson <dean@pleasingsoftware.com>
2015-11-23 15:30:33 -0700

    Removed commented out section

--- arduino/input/lidarservo.js
+++ arduino/input/lidarservo.js
@@ -16,13 +16,6 @@ board.on("ready", function() {
 
     servo.center();
 
-    //proximity.on("data", function() {
-    //    console.log("Proximity: ");
-    //    console.log("  cm  : ", this.cm);
-    //    console.log("  in  : ", this.in);
-    //    console.log("-----------------");
-    //});
-
     proximity.on("change", function() {
         if (this.cm < 20 && !hasPlayed) {
             piezo.play(sw);
