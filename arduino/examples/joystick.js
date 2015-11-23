7b4af16a76c1e71d84ae67415b3f9a40fa42fc82
Dean Iverson <dean@pleasingsoftware.com>
2015-11-23 14:34:35 -0700

    Added new demo

--- arduino/examples/joystick.js
+++ arduino/examples/joystick.js
@@ -16,7 +16,7 @@ five.Board().on("ready", function() {
     five.Servos.center();
 
     joystick.on('change', function() {
-        tilt.move(Math.ceil(180 * this.fixed.y));
-        pan.move(Math.ceil(180 * this.fixed.x));
+        tilt.move(Math.ceil(180 * this.y));
+        pan.move(Math.ceil(180 * this.x));
     })
 });
---
+++ arduino/input/lidarservo.js
@@ -0,0 +1,35 @@
+var five = require("johnny-five");
+var songs = require("j5-songs")
+
+var board = new five.Board();
+
+board.on("ready", function() {
+    var servo = five.Servo(10);
+
+    var piezo = new five.Piezo(3),
+        sw = songs.load('starwars-theme'),
+        hasPlayed = false;
+
+    var proximity = new five.Proximity({
+        controller: "LIDARLITE"
+    });
+
+    servo.center();
+
+    //proximity.on("data", function() {
+    //    console.log("Proximity: ");
+    //    console.log("  cm  : ", this.cm);
+    //    console.log("  in  : ", this.in);
+    //    console.log("-----------------");
+    //});
+
+    proximity.on("change", function() {
+        if (this.cm < 20 && !hasPlayed) {
+            piezo.play(sw);
+            hasPlayed = true;
+        }
+
+        console.log(this.cm + "cm");
+        servo.to(Math.min((this.cm - 15)*6, 180));
+    });
+});
